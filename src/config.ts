12a78d10820b1e0d6d3c84c9668352d16976a5f4
Doroshokkk <vladorchikz@gmail.com>
2023-09-01 18:39:10 +0300

    corrected config.ts

--- src/config.ts
+++ src/config.ts
@@ -26,8 +26,8 @@ const config: AppConfig = {
     dialect: 'postgres',
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT || '5432'),
-    username: process.env.DB_USERNAME || 'username',
-    password: process.env.DB_PASSWORD || 'password',
+    username: process.env.DB_USERNAME || 'myuser',
+    password: process.env.DB_PASSWORD || 'mypassword',
     database: process.env.DB_NAME || 'mydatabase'
   },
   redis: {
